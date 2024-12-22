61d3f5307a4c84327ece483fa384e583ab397d92
Dory <pemegomachado@gmail.com>
2024-12-22 22:52:44 +0100

    Corrected tabulation in window.hpp

--- src/window/window.hpp
+++ src/window/window.hpp
@@ -8,34 +8,34 @@
 
 class Window {
 public:
-    Window(int width, int height, const char* title);
-    ~Window() {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+  Window(int width, int height, const char* title);
+  ~Window() {
+      glfwDestroyWindow(window);
+      glfwTerminate();
+  }
 
-    void clear(float r, float g, float b, float a) {
-        glClearColor(r, g, b, a);
-        glClear(GL_COLOR_BUFFER_BIT);
-    }
+  void clear(float r, float g, float b, float a) {
+      glClearColor(r, g, b, a);
+      glClear(GL_COLOR_BUFFER_BIT);
+  }
 
-    void swapBuffers() {
-        glfwSwapBuffers(window);
-    }
+  void swapBuffers() {
+      glfwSwapBuffers(window);
+  }
 
-    void pollEvents() {
-        glfwPollEvents();
-    }
+  void pollEvents() {
+      glfwPollEvents();
+  }
 
-    bool shouldClose() {
-        return glfwWindowShouldClose(window);
-    }
+  bool shouldClose() {
+      return glfwWindowShouldClose(window);
+  }
 private:
-    GLFWwindow* window;
+  GLFWwindow* window;
 
-    static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
-        glViewport(0, 0, width, height);
-    }
+  static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+      glViewport(0, 0, width, height);
+  }
 };
 
 #endif // WINDOW_HPP
