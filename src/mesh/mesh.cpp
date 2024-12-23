1df80df1482adbff35e5d9d32abcb77591f301f5
Dory <pemegomachado@gmail.com>
2024-12-23 11:43:40 +0100

    Abstracted rendering code

---
+++ src/mesh/mesh.cpp
@@ -0,0 +1,28 @@
+#include "mesh.hpp"
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, Shader shader) :
+  vertices(vertices), indices(indices), shader(shader) {
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  // Bind VAO
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+}
+
+void Mesh::render() {
+  shader.use();
+  glBindVertexArray(VAO);
+  glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+  glBindVertexArray(0);
+}
---
+++ src/mesh/mesh.hpp
@@ -0,0 +1,43 @@
+#ifndef MESH_HPP
+#define MESH_HPP
+
+#include <glad/glad.h>
+
+#include <vector>
+
+#include "../shader/shader.hpp"
+
+class Mesh {
+public:
+  struct Vertex {
+    float x, y, z;
+
+    Vertex() {
+      x = 0.0f;
+      y = 0.0f;
+      z = 0.0f;
+    }
+    Vertex(float x, float y, float z) {
+      this->x = x;
+      this->y = y;
+      this->z = z;
+    }
+  };
+
+  Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, Shader shader);
+  ~Mesh() {
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+  }
+
+  void render();
+
+private:
+  unsigned int VAO, VBO, EBO;
+  std::vector<Vertex> vertices;
+  std::vector<unsigned int> indices;
+  Shader shader;
+};
+
+#endif // MESH_HPP
