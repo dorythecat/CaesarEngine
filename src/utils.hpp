f14ae51383b950b7bbe32927995d03c32be64a6f
Dory <pemegomachado@gmail.com>
2025-03-22 12:29:33 +0100

    Updated utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -2,8 +2,15 @@
 #define UTILS_HPP
 
 typedef struct {
-    int x;
-    int y;
+  int x, y;
 } vec2i;
+
+typedef struct {
+  float x, y;
+} vec2f;
+
+typedef struct {
+  double x, y;
+} vec2d;
 
 #endif // UTILS_HPP
