549f2c80f7c6366535d8e37f7abe7129e37915b5
Dory <pemegomachado@gmail.com>
2025-03-28 18:16:20 +0100

    Added printException() function

--- src/utils.hpp
+++ src/utils.hpp
@@ -13,4 +13,13 @@ typedef struct {
   double x, y;
 } vec2d;
 
+void printException(const std::exception& e, int level = 0) {
+  std::cerr << std::string(level, ' ') << "ERROR: " << e.what() << std::endl;
+  try {
+    std::rethrow_if_nested(e);
+  } catch (const std::exception& e) {
+    printException(e, level + 1);
+  } catch (...) {}
+}
+
 #endif // UTILS_HPP
