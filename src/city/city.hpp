45544c138af1f106dfa450c4c4b57754f062070c
Dory <pemegomachado@gmail.com>
2025-04-13 11:34:32 +0200

    Updated city class, and added cities.txt

--- src/city/city.hpp
+++ src/city/city.hpp
@@ -2,6 +2,7 @@
 #define CITY_HPP
 
 #include <string>
+#include <vector>
 
 #include "../utils.hpp"
 #include "../province/province.hpp"
@@ -16,18 +17,20 @@ public:
     int wealth;
     int food;
     int industry;
+    bool isCapital = false;
   } Data;
 
   City(Data data) : data(data) {}
   ~City() = default;
 
   std::string getName() const { return data.name; }
-  Province &getProvince() const { return data.province; }
+  Province& getProvince() const { return data.province; }
   vec2f getPosition() const { return data.position; }
   int getPopulation() const { return data.population; }
   int getWealth() const { return data.wealth; }
   int getFood() const { return data.food; }
   int getIndustry() const { return data.industry; }
+  bool isCapital() const { return data.isCapital; }
 
 private:
   Data data;
