19cce1662c74076968a1113f07f71b007cf50b6c
Dory <pemegomachado@gmail.com>
2025-08-16 20:38:02 +0200

    Re-added erroneously removed line

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -10,6 +10,8 @@ Window::Window(const int width, const int height, const char* title) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); // Make window not resizable
+
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
