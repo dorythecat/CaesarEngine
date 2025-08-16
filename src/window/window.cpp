096546d7c13b5a12056e59888423ff4cf1152071
Dory <pemegomachado@gmail.com>
2025-08-16 19:50:22 +0200

    Imrpoved window class

--- src/window/window.cpp
+++ src/window/window.cpp
@@ -1,33 +1,34 @@
 #include "window.hpp"
 
-Window::Window(int width, int height, const char* title) {
-  if (!glfwInit()) {
-    std::cerr << "Failed to initialize GLFW" << std::endl;
-    exit(EXIT_FAILURE);
-  }
+Window::Window(const int width, const int height, const char* title) {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Use OpenGL version 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-  _window = glfwCreateWindow(width, height, title, nullptr, nullptr);
-  if (!_window) {
-    std::cerr << "Failed to create window" << std::endl;
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
+    _window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    if (!_window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-  glfwMakeContextCurrent(_window);
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cerr << "Failed to initialize GLAD" << std::endl;
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
+    glfwMakeContextCurrent(_window);
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 
-  glViewport(0, 0, width, height);
-  glfwSetFramebufferSizeCallback(_window, framebufferSizeCallback);
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(_window, framebufferSizeCallback);
 }
--- src/window/window.hpp
+++ src/window/window.hpp
@@ -4,51 +4,41 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-
 #include "../utils.hpp"
 
 class Window {
 public:
-  Window(int width, int height, const char* title);
-  ~Window() {
-      glfwDestroyWindow(_window);
-      glfwTerminate();
-  }
+    Window(int width, int height, const char* title);
+    ~Window() {
+        glfwDestroyWindow(_window);
+        glfwTerminate();
+    }
 
-  void clear(float r, float g, float b, float a) {
-      glClearColor(r, g, b, a);
-      glClear(GL_COLOR_BUFFER_BIT);
-  }
+    static void clear(const float r, const float g, const float b, const float a) {
+        glClearColor(r, g, b, a);
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
 
-  void swapBuffers() {
-      glfwSwapBuffers(_window);
-  }
+    void swapBuffers() const { glfwSwapBuffers(_window); }
 
-  void pollEvents() {
-      glfwPollEvents();
-  }
+    static void pollEvents() { glfwPollEvents(); }
 
-  bool shouldClose() {
-      return glfwWindowShouldClose(_window);
-  }
+    [[nodiscard]] bool shouldClose() const { return glfwWindowShouldClose(_window) == GLFW_TRUE; }
 
-  GLFWwindow* window() {
-      return _window;
-  }
+    [[nodiscard]] GLFWwindow* window() const { return _window; }
 
-  vec2i getDimensions() {
-      vec2i dimensions;
-      glfwGetWindowSize(_window, &dimensions.x, &dimensions.y);
-      return dimensions;
-  }
+    [[nodiscard]] vec2i getDimensions() const {
+        vec2i dimensions;
+        glfwGetWindowSize(_window, &dimensions.x, &dimensions.y);
+        return dimensions;
+    }
 
 private:
-  GLFWwindow* _window;
+    GLFWwindow* _window;
 
-  static void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
-      glViewport(0, 0, width, height);
-  }
+    static void framebufferSizeCallback(GLFWwindow* window, const int width, const int height) {
+        glViewport(0, 0, width, height);
+    }
 };
 
 #endif // WINDOW_HPP
