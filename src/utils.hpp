6d6ef9de1e35373dd39cac9ae9e26cee59d8efbc
dory <pemegomachado@gmail.com>
2025-08-24 18:48:10 +0200

    Corrected a type issue

--- src/utils.hpp
+++ src/utils.hpp
@@ -23,8 +23,8 @@ struct vec2 {
     }
   
     // Comparison
-    vec2 operator==(const vec2& other) const { return x == other.x && y == other.y; }
-    vec2 operator!=(const vec2& other) const { return x != other.x || y != other.y; }
+    bool operator==(const vec2& other) const { return x == other.x && y == other.y; }
+    bool operator!=(const vec2& other) const { return x != other.x || y != other.y; }
 
     // Basic arithmetic
     vec2 operator+(const vec2& other) const { return vec2(x + other.x, y + other.y); }
