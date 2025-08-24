4ff4b025291b6a383196ba01ee911860b4347010
dory <pemegomachado@gmail.com>
2025-08-24 22:22:15 +0200

    Updated Error Handler

--- src/error_handler/error_handler.h
+++ src/error_handler/error_handler.h
@@ -2,6 +2,7 @@
 #define ERROR_HANDLER_H
 
 #include <string>
+#include <vector>
 #include <iostream>
 
 // Just a simple error handler that can be set for different warning levels
@@ -17,9 +18,18 @@ public:
 
   enum ErrorCode {
     UNKNOWN_ERROR = 0,
-    FILE_NOT_FOUND = 1,
-    COULD_NOT_OPEN_FILE = 2,
-    INVALID_FORMAT = 3
+    WINDOW_CREATION_ERROR = 1,
+    FILE_NOT_FOUND_ERROR = 2,
+    COULD_NOT_OPEN_FILE_ERROR = 3,
+    FORMAT_ERROR = 4
+  };
+
+  std::vector<std::string> errorMessages = {
+    "CODE 0 | Unknown error",
+    "CODE 1 | Window creation error",
+    "CODE 2 | File not found error",
+    "CODE 3 | Could not open file error",
+    "CODE 4 | Format error"
   };
 
   explicit ErrorHandler(const LogLevel logLevel = ALL) : logLevel(logLevel) {}
@@ -33,20 +43,20 @@ public:
 
   void logFatal(const std::string &message, const ErrorCode errorCode = UNKNOWN_ERROR) const {
     // Every fatal error should be logged
-    std::cerr << "[FATAL] (" << errorCode << "): " << message << std::endl;
+    std::cerr << "[FATAL] (" << errorMessages[errorCode] << "): " << message << std::endl;
     std::exit(EXIT_FAILURE);
   }
   void logError(const std::string &message, const ErrorCode errorCode = UNKNOWN_ERROR) const {
     if (!(logLevel & ERROR)) return;
-    std::cerr << "[ERROR] (" << errorCode << "): " << message << std::endl;
+    std::cerr << "[ERROR] (" << errorMessages[errorCode] << "): " << message << std::endl;
   }
   void logWarning(const std::string &message, const ErrorCode errorCode = UNKNOWN_ERROR) const {
     if (!(logLevel & WARNING)) return;
-    std::cerr << "[WARNING] (" << errorCode << "): " << message << std::endl;
+    std::cerr << "[WARNING] (" << errorMessages[errorCode] << "): " << message << std::endl;
   }
   void logInfo(const std::string &message, const ErrorCode errorCode = UNKNOWN_ERROR) const {
     if (!(logLevel & INFO)) return;
-    std::cout << "[INFO] (" << errorCode << "): " << message << std::endl;
+    std::cout << "[INFO] (" << errorMessages[errorCode] << "): " << message << std::endl;
   }
 
 private:
