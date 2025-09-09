4c73fdcc477f55f967d10fe5cc488d6cbb0e8046
dorythecat <pemegomachado@gmail.com>
2025-09-09 12:10:18 +0200

    Implemented the line class

--- src/line/line.cpp
+++ src/line/line.cpp
@@ -1,1 +1,66 @@
 #include "line.h"
+
+#include <utility>
+
+Line::Line(ErrorHandler *errorHandler,
+           std::vector<vec2f> points,
+           const std::string &shaderPath) : shader(errorHandler, shaderPath),
+                                            errorHandler(errorHandler) {
+  generateMesh(points);
+  generateMeshData();
+}
+
+void Line::render(const Window &window, float scale, const vec2f &offset) const {
+  glBindVertexArray(VAO);
+  glDrawElements(GL_LINES,
+                 static_cast<int>(indices.size()),
+                 GL_UNSIGNED_INT,
+                 nullptr);
+  glBindVertexArray(0);
+}
+
+void Line::generateMesh(std::vector<vec2f> points) {
+  if (points.size() < 2) {
+    errorHandler->logError("Line must have at least 2 points");
+    return;
+  }
+
+  // Very, very basic line generation
+  vertices = std::move(points);
+  indices.reserve((vertices.size() - 1) * 2);
+  for (unsigned int i = 0; i < vertices.size() - 1; i++) {
+    indices.push_back(i);
+    indices.push_back(i + 1);
+  }
+}
+
+void Line::generateMeshData() {
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  // Bind VAO
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER,
+               static_cast<GLsizeiptr>(vertices.size() * sizeof(vec2f)),
+               &vertices[0],
+               GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+               static_cast<GLsizeiptr>(indices.size() * sizeof(unsigned int)),
+               &indices[0],
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0,
+                        2,
+                        GL_FLOAT,
+                        GL_FALSE,
+                        sizeof(vec2f),
+                        nullptr);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindVertexArray(0);
+}
--- src/line/line.h
+++ src/line/line.h
@@ -1,26 +1,34 @@
 #ifndef LINE_H
 #define LINE_H
 
+#include <glad/glad.h>
+
 #include <vector>
 
 #include "../utils.hpp"
 #include "../error_handler/error_handler.h"
 #include "../window/window.hpp"
+#include "../shader/shader.hpp"
 
 class Line {
 public:
-  explicit Line(ErrorHandler* errorHandler,
-                const std::string& shaderPath = "res/shaders/default");
+  Line(ErrorHandler* errorHandler,
+       std::vector<vec2f> points,
+       const std::string& shaderPath = "res/shaders/line");
   ~Line() = default;
 
-  void addPoint(const vec2f& point) {
-    points.push_back(point);
-  }
-
-  void render(const Window& window, float scale, const vec2f& offset);
+  void render(const Window& window, float scale, const vec2f& offset) const;
 
 private:
-  std::vector<vec2f> points;
+  unsigned int VAO{}, VBO{}, EBO{};
+  std::vector<vec2f> vertices;
+  std::vector<unsigned int> indices;
+  Shader shader;
+
+  ErrorHandler* errorHandler;
+
+  void generateMesh(std::vector<vec2f> points);
+  void generateMeshData();
 };
 
 
