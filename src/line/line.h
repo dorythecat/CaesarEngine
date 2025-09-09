663cc9ba0b6e9193573e98de5e6c9e699b209590
dorythecat <pemegomachado@gmail.com>
2025-09-09 11:18:05 +0200

    Populated line.h

--- src/line/line.h
+++ src/line/line.h
@@ -1,8 +1,26 @@
 #ifndef LINE_H
 #define LINE_H
 
+#include <vector>
 
-class line {
+#include "../utils.hpp"
+#include "../error_handler/error_handler.h"
+#include "../window/window.hpp"
+
+class Line {
+public:
+  explicit Line(ErrorHandler* errorHandler,
+                const std::string& shaderPath = "res/shaders/default");
+  ~Line() = default;
+
+  void addPoint(const vec2f& point) {
+    points.push_back(point);
+  }
+
+  void render(const Window& window, float scale, const vec2f& offset);
+
+private:
+  std::vector<vec2f> points;
 };
 
 
