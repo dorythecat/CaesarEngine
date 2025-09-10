1f831781d05afd9988100dab90182583e8e0f0b8
dorythecat <pemegomachado@gmail.com>
2025-09-10 12:52:01 +0200

    Moved Catmull-Rom code to .cpp

--- src/line/line.cpp
+++ src/line/line.cpp
@@ -63,3 +63,26 @@ void Line::addSegment(const vec2f &start, const vec2f &end) {
   vertices.push_back(end + perpendicular);
   vertices.push_back(end - perpendicular);
 }
+
+vec2f Line::catmullRom(const vec2f &p0, const vec2f &p1, const vec2f &p2, const vec2f &p3, float t) {
+  // See https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+
+  // Knot parameters
+  constexpr float t0 = 0.0f;
+  const float t1 = t0 + std::pow((p1 - p0).length(), 0.5f);
+  const float t2 = t1 + std::pow((p2 - p1).length(), 0.5f);
+  const float t3 = t2 + std::pow((p3 - p2).length(), 0.5f);
+
+  // Rescale t to the range [t1, t2], so it goes from P1 to P2
+  t = t1 + t * (t2 - t1);
+
+  // Calculate the basis functions
+  const vec2f A1 = p0 * (t1 - t) / (t1 - t0) + p1 * (t - t0) / (t1 - t0);
+  const vec2f A2 = p1 * (t2 - t) / (t2 - t1) + p2 * (t - t1) / (t2 - t1);
+  const vec2f A3 = p2 * (t3 - t) / (t3 - t2) + p3 * (t - t2) / (t3 - t2);
+
+  const vec2f B1 = A1 * (t2 - t) / (t2 - t0) + A2 * (t - t0) / (t2 - t0);
+  const vec2f B2 = A2 * (t3 - t) / (t3 - t1) + A3 * (t - t1) / (t3 - t1);
+
+  return B1 * (t2 - t) / (t2 - t1) + B2 * (t - t1) / (t2 - t1); // Final point
+}
