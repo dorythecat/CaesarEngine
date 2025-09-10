3e21b044152c46087e74e5a0d4e9148986e37ea5
dorythecat <pemegomachado@gmail.com>
2025-09-10 13:30:41 +0200

    Fixed issues with the curvature of lines

--- src/line/line.cpp
+++ src/line/line.cpp
@@ -22,10 +22,10 @@ void Line::generateMesh(const std::vector<vec2f> &points) {
   // Catmull-Rom spline for smooth lines
   const size_t n = points.size();
   for (size_t i = 0; i < n - 1; i++) {
-    const vec2f& p0 = (i == 0) ? points[i].normalized() : points[i - 1];
+    const vec2f& p0 = (i == 0) ? points[i] + (points[i] - points[i + 1]).normalized() : points[i - 1];
     const vec2f& p1 = points[i];
     const vec2f& p2 = points[i + 1];
-    const vec2f& p3 = (i + 2 < n) ? points[i + 2] : points[i + 1].normalized();
+    const vec2f& p3 = (i + 2 < n) ? points[i + 2] : points[i + 1] + (points[i + 1] - points[i]).normalized();
     for (int j = 0; j < CURVE_SEGMENTS; j++) {
       const float t = static_cast<float>(j) * CURVE_SEGMENTS_INVERSE;
       vec2f pointA = catmullRom(p0, p1, p2, p3, t) * 2.0f; // Scale up because we're in NDC
