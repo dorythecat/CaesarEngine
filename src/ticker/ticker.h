c583eeb27a52d7fb5d8e312208ca88368837c7f9
dorythecat <pemegomachado@gmail.com>
2025-09-29 21:50:15 +0200

    Added tick callbacks support

--- src/ticker/ticker.h
+++ src/ticker/ticker.h
@@ -1,6 +1,8 @@
 #ifndef TICKER_H
 #define TICKER_H
 
+#include <vector>
+
 #include "../error_handler/error_handler.h"
 
 class Ticker {
@@ -8,15 +10,19 @@ public:
     explicit Ticker(ErrorHandler* errorHandler) : errorHandler(errorHandler) {}
     ~Ticker() = default;
 
+    void registerTickCallback(void(*callback)()) { tickCallbacks.push_back(callback); }
     void tick() {
         tickCounter++;
         errorHandler->logInfo("Tick");
+        for (const auto& callback : tickCallbacks) callback();
     }
     [[nodiscard]] unsigned long getTick() const { return tickCounter; }
 
 private:
     ErrorHandler* errorHandler;
     unsigned long tickCounter = 0; // Current tick
+
+    std::vector<void(*)()> tickCallbacks; // Functions to call on each tick
 };
 
 #endif //TICKER_H
